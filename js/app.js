107c9440dbb41fe7dc1ab73fc5d5687810b87baa
Mcriado <mcriado.cg@gmail.com>
2020-06-26 00:10:59 -0400

    Trabajando con métodos en arrays

--- js/app.js
+++ js/app.js
@@ -25,13 +25,16 @@ const dental = [
    {hora: '14:00', especialista: 'Raquel Villaseca', paciente: 'Ana Sepulveda', rut: '14441281-0', prevision: 'ISAPRE'},
 ]
 
+traumatologia.unshift({hora: '09:00', especialista: 'René Poblete', paciente: 'Ana Gellona', rut: '13123329-7', prevision: 'ISAPRE'}, {hora: '09:30', especialista: 'María Solar', paciente: 'Ramiro Andrade', rut: '12221451-k', prevision: 'FONASA'}, {hora: '10:00', especialista: 'Raul Loyola', paciente: 'Carmen Isla', rut: '10112348-3', prevision: 'ISAPRE'}, {hora: '10:30', especialista: 'Antonio Larenas', paciente: 'Pablo Loayza', rut: '13453234-1', prevision: 'ISAPRE'}, {hora: '12:00', especialista: 'Matías Aravena', paciente: 'Susana Poblete', rut: '14345656-6', prevision: 'FONASA'});
+radiologia.pop();
+radiologia.shift();
+
 const radioConsultas = document.querySelector('.stats__length-r');
 const trauConsultas = document.querySelector('.stats__length-t');
 const dentalConsultas = document.querySelector('.stats__length-d');
 const radioAtenciones = document.querySelector('.stats__first-atention-r');
 const trauAtenciones = document.querySelector('.stats__first-atention-t');
 const dentalAtenciones = document.querySelector('.stats__first-atention-d');
-
 
 insertarConsultas()
 function insertarConsultas() {
@@ -75,9 +78,34 @@ function insertarAtenciones() {
    document.querySelector('.stats__last-atention-d').appendChild(lastD);
 } 
 
-traumatologia.push({hora: '09:00', especialista: 'René Poblete', paciente: 'Ana Gellona', rut: '13123329-7', prevision: 'ISAPRE'}, {hora: '09:30', especialista: 'María Solar', paciente: 'Ramiro Andrade', rut: '12221451-k', prevision: 'FONASA'}, {hora: '10:00', especialista: 'Raul Loyola', paciente: 'Carmen Isla', rut: '10112348-3', prevision: 'ISAPRE'}, {hora: '10:30', especialista: 'Antonio Larenas', paciente: 'Pablo Loayza', rut: '13453234-1', prevision: 'ISAPRE'}, {hora: '12:00', especialista: 'Matías Aravena', paciente: 'Susana Poblete', rut: '14345656-6', prevision: 'FONASA'});
-radiologia.pop();
-radiologia.shift();
+dental.forEach(function(element){
+   let text = document.createElement('p');
+   text.textContent = `${element.hora} - ${element.especialista} - ${element.paciente} - ${element.rut} - ${element.prevision}`
+   document.querySelector('.details__pacient-dental').appendChild(text)
+});
+
+changePrevision = dental.map(function(element){
+   if(element.prevision === 'FONASA') {
+      element.prevision = 'ISAPRE'
+   } else {
+      element.prevision = 'FONASA'
+   }
+   return element;
+});
+
+changePrevision.forEach(function(element){
+   let text = document.createElement('p');
+   text.textContent = `${element.prevision} - ${element.rut}`;
+   document.querySelector('.details__pacient-prevision').appendChild(text);
+});
+
+var consultasTotales = [];
+consultasTotales = radiologia.concat(traumatologia, dental);
+consultasTotales.forEach(function(element){
+   let text = document.createElement('p');
+   text.textContent = `Nombre paciente: ${element.paciente}`;
+   document.querySelector('.details__all-names-consult').appendChild(text);
+});
 
 console.log(radiologia)
 console.log(traumatologia)
